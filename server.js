4105a93ec581e30032161d015f07792ae0a49a85
Erick John Orqueza Lopez <112525487+CyberWhisker@users.noreply.github.com>
2025-03-07 10:14:06 +0800

    Done Order and Transaction Crud

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const UserRoutes = require('./routes/user')
 const TechStackRoutes = require('./routes/techStack')
 const InventoryRoutes = require('./routes/inventory')
 const ProjectRoutes = require('./routes/project')
+const TransactionRoutes = require('./routes/transaction')
+const OrderRoutes = require('./routes/order')
 
 const port = process.env.PORT
 
@@ -31,6 +33,8 @@ app.use('/api/user', UserRoutes)
 app.use('/api/techStack', TechStackRoutes)
 app.use('/api/inventory', InventoryRoutes)
 app.use('/api/project', ProjectRoutes)
+app.use('/api/transaction', TransactionRoutes)
+app.use('/api/order', OrderRoutes)
 
 // Connect to db
 mongoose.connect(process.env.MONGO_URI)
